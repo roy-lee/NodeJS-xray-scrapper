deb4aeb87ca7603f8c76823656d635130545b58c
Roy Lee <1700500@sit.singaporetech.edu.sg>
2018-05-09 10:53:10 +0800

    adding up multiple twitter links
    preparation for multiple scrape

--- app/SMRT_singapore.js
+++ app/SMRT_singapore.js
@@ -7,11 +7,30 @@ var x = Xray({
   }
 });
 
-var scrapeLink = "https://twitter.com/smrt_singapore";
+var SingaporeTransportTweets = [
+  "https://twitter.com/smrt_singapore",
+  "https://twitter.com/ltatrafficnews",
+  "https://twitter.com/sbstransit_ltd"
+]
 
-x(scrapeLink , 'div.tweet', [{
-  title: 'strong.fullname',
-  tweet: '.tweet-text',
-  link: '@data-permalink-path | add'
-}])(console.log)
-    .write('results/SMRT_singapore.json')
+for (var i = 0; i < SingaporeTransportTweets.length; i++) {
+  scrapeLink = SingaporeTransportTweets[i]
+
+  try {
+    x(scrapeLink, 'div.tweet', [{
+      title: 'strong.fullname',
+      tweet: '.tweet-text',
+      link: '@data-permalink-path | add'
+    }])(console.log)
+        .write('results/SMRT_singapore.json');
+  }
+  /* TO-DO:
+  1. Stop scrapping after 25th results
+  2. Append JSON instead of overwriting file
+  3. Exception handling
+  */
+  catch(err) {
+    console.log("##### Reached end of page. #####");
+  }
+
+}
